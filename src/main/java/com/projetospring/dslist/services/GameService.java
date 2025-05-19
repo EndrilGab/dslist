185b842b357515e3e3270cea6b824f6956118c40
EndrilGab <endrilgabriel26@gmail.com>
2025-05-19 17:20:54 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/projetospring/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.projetospring.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetospring.dslist.dto.GameMinDTO;
+import com.projetospring.dslist.entities.Game;
+import com.projetospring.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
