94c909698f4dfac8347427d857dcafee756ecacb
EndrilGab <endrilgabriel26@gmail.com>
2025-05-21 03:10:27 -0300

    Consulta sql, projection, GET game by list

--- src/main/java/com/projetospring/dslist/services/GameService.java
+++ src/main/java/com/projetospring/dslist/services/GameService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.projetospring.dslist.dto.GameDTO;
 import com.projetospring.dslist.dto.GameMinDTO;
 import com.projetospring.dslist.entities.Game;
+import com.projetospring.dslist.projections.GameMinProjection;
 import com.projetospring.dslist.repositories.GameRepository;
 
 @Service
@@ -32,4 +33,11 @@ public class GameService {
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
 }
